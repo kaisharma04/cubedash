0a43deba61e7ae8e2df696badf26d31974b9e02a
Krishanu Sharma <krishanu.sharma619@gmail.com>
2018-06-08 01:10:02 -0700

    adding game files

---
+++ Assets/scripts/playerCollision.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class playerCollision : MonoBehaviour {
+ public playermovement movement;
+ 
+void OnCollisionEnter (Collision collisionInfo)
+{
+	if(collisionInfo.collider.tag =="obstacle")
+	{
+		movement.enabled = false;
+		FindObjectOfType<GameManager>().EndGame();
+	}
+}
+}
---
+++ Assets/scripts/playermovement.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+
+public Rigidbody rb;
+public float forwardforce=2000f;
+public float sidewaysforce=500f;
+	
+	// we marked this as "Fixed"Update coz we
+	//are using it to mess with physics
+	void FixedUpdate ()
+	 {
+       rb.AddForce(0,0,forwardforce*Time.deltaTime);
+
+	   if(Input.GetKey("d"))
+	   {
+		   	rb.AddForce(sidewaysforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+	   }
+	    if(Input.GetKey("a"))
+	   {
+		   	rb.AddForce(-sidewaysforce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+	   }
+		
+		if(rb.position.y < -1f)
+		{
+			FindObjectOfType<GameManager>().EndGame();
+		}
+	}
+}
